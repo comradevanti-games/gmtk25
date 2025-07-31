299f754c5affd777f27b23888508150fd87d06cb
Ramon Brullo <kingvanti650@gmail.com>
2025-07-31 17:38:07 +0200

    add died event to health

--- Assets/Code/HealthKeeper.cs
+++ Assets/Code/HealthKeeper.cs
@@ -1,16 +1,31 @@
+using System;
 using UnityEngine;
 
 namespace GMTK25
 {
     public sealed class HealthKeeper : MonoBehaviour
     {
-        [SerializeField] private float initialHealth;
+        public event Action? Died;
 
-        public float Health { get; private set; }
+        [SerializeField] private float initialHealth;
+        private float health;
+
+        public float Health
+        {
+            get => health;
+            private set
+            {
+                if (Mathf.Approximately(value, health)) return;
+
+                health = Mathf.Max(value, 0);
+
+                if (health == 0) Died?.Invoke();
+            }
+        }
 
         public void TakeDamage(float amount)
         {
-            Health = Mathf.Max(Health - amount, 0);
+            Health -= amount;
         }
 
         private void Awake()
