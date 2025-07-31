005dcf5965836b1b72506fb779647f390c9db23e
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2025-07-31 10:45:19 +0200

    add input handler

--- Assets/Code/InputHandler.cs
+++ Assets/Code/InputHandler.cs
@@ -1,11 +1,19 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace GMTK25 {
 
     public class InputHandler : MonoBehaviour {
 
-        
-        
+        public event Action<Vector2> MovementInputHandled;
+
+        public void OnMovementInputReceived(InputAction.CallbackContext ctx) {
+
+            Vector2 value = ctx.ReadValue<Vector2>();
+            MovementInputHandled?.Invoke(value);
+
+        }
 
     }
 
