d4ad2b002ca0f01d84914d0789b6a96f977faee5
Ramon Brullo <kingvanti650@gmail.com>
2025-07-31 14:14:03 +0200

    add initial logic for ejecting bullet

--- Assets/Code/RevolverDrumKeeper.cs
+++ Assets/Code/RevolverDrumKeeper.cs
@@ -6,6 +6,19 @@ namespace GMTK25
     {
         [SerializeField] private BulletType initialBulletType = null!;
 
-        public BulletType? ChamberedBulletType => initialBulletType;
+        public BulletType? ChamberedBulletType { get; private set; }
+
+        /// <summary>
+        /// Ejects the current <see cref="ChamberedBulletType"/> (if any).
+        /// </summary>
+        public void EjectBullet()
+        {
+            ChamberedBulletType = null;
+        }
+
+        private void Awake()
+        {
+            ChamberedBulletType = initialBulletType;
+        }
     }
 }
