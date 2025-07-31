30f02ca04e4ec73e9956506e2af4ed30935ce16f
Ramon Brullo <kingvanti650@gmail.com>
2025-07-31 13:34:12 +0200

    add enemy-groups to sub-waves

--- Assets/Code/WaveDescription.cs
+++ Assets/Code/WaveDescription.cs
@@ -5,9 +5,25 @@ using UnityEngine;
 namespace GMTK25
 {
     [Serializable]
+    public class EnemyGroup
+    {
+        [SerializeField] private EnemyType type = null!;
+        [SerializeField] private int count;
+
+        public EnemyType Type => type;
+
+        public int Count => count;
+    }
+
+    [Serializable]
     public class SubWave
     {
+        [SerializeField]
+        private EnemyGroup[] groups = Array.Empty<EnemyGroup>();
+
         [SerializeField] private float durationSeconds;
+
+        public IReadOnlyCollection<EnemyGroup> Groups => groups;
 
         public TimeSpan Duration => TimeSpan.FromSeconds(durationSeconds);
     }
