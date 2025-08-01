98b932eef97cebbf6ed5352a5b990d44fc0168ce
Ramon Brullo <kingvanti650@gmail.com>
2025-08-01 09:50:18 +0200

    add health changed event

--- Assets/Code/HealthKeeper.cs
+++ Assets/Code/HealthKeeper.cs
@@ -6,6 +6,7 @@ namespace GMTK25
 {
     public sealed class HealthKeeper : MonoBehaviour
     {
+        public UnityEvent<float> changed = new UnityEvent<float>();
         public UnityEvent died = new UnityEvent();
 
         [SerializeField] private float initialHealth;
@@ -17,9 +18,12 @@ namespace GMTK25
             get => health;
             private set
             {
-                if (Mathf.Approximately(value, health)) return;
+                var newHealth = Mathf.Max(value, 0);
 
-                health = Mathf.Max(value, 0);
+                if (Mathf.Approximately(newHealth, health)) return;
+                health = newHealth;
+
+                changed.Invoke(health);
 
                 if (health != 0) return;
 
