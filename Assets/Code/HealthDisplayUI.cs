880ed903f9327963c9995cd6b1c3400e45d513e2
Mumb0 <55690191+Mumb0@users.noreply.github.com>
2025-08-01 13:39:06 +0200

    drop unused var

--- Assets/Code/HealthDisplayUI.cs
+++ Assets/Code/HealthDisplayUI.cs
@@ -6,7 +6,6 @@ namespace GMTK25 {
     public class HealthDisplayUI : MonoBehaviour {
 
         [SerializeField] private Image heartImage = null!;
-        [SerializeField] private int initialHealth = 3;
 
         public void OnHealthChanged(float newHealth) {
             heartImage.fillAmount = (System.Convert.ToSingle(1) / 3) * (int)newHealth;
