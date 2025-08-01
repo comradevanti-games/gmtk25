6399a7146c45b236a6dffb3ce57d795d53da0d1b
Ramon Brullo <kingvanti650@gmail.com>
2025-08-01 13:03:13 +0200

    add empty shop keeper

---
+++ Assets/Code/ShopKeeper.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace GMTK25
+{
+    public sealed class ShopKeeper : MonoBehaviour
+    {
+        public void OnEnable()
+        {
+        }
+
+        private void OnDisable()
+        {
+        }
+    }
+}
