b32c672dfd5f469d937f9549e06409512ec0f951
Ramon Brullo <kingvanti650@gmail.com>
2025-08-03 11:39:54 +0200

    add sniper bullet

---
+++ Assets/Code/Bullets/Sniper/DistanceReturnBehavior.cs
@@ -0,0 +1,25 @@
+using System;
+using GMTK25.Bullets.Return;
+using UnityEngine;
+
+namespace GMTK25.Bullets
+{
+    public sealed class DistanceReturnBehavior : MonoBehaviour, IReturnFilter
+    {
+        [SerializeField] private float minDistance;
+
+        private Vector2 startPosition;
+
+        private void Start()
+        {
+            startPosition = transform.position;
+        }
+
+        public bool ShouldReturn(BulletHit hit)
+        {
+            var endPosition = transform.position;
+            var distance = Vector2.Distance(startPosition, endPosition);
+            return distance >= minDistance;
+        }
+    }
+}
